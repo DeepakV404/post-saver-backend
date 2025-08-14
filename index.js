1894d48926125f03b1a3d50ae4b737a731c6cc6c
Deepak <17deepakv5@gmail.com>
2025-08-14 20:55:25 +0530

    chore: init node app

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.json({ status: 'ok' });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`);
+});
+
+
