62b95714e6e50a1e2a8b8c857b98839f39536c84
Deepak <17deepakv5@gmail.com>
2025-08-19 23:07:54 +0530

    chore: init posts middleware and user middleware

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -3,19 +3,5 @@ import postController from '../controllers/postController.js';
 
 const postRouter = express.Router();
 
-// GET /api/posts - Get all posts
-postRouter.get('/', postController.getAllPosts);
-
-// GET /api/posts/:id - Get post by ID
-postRouter.get('/:id', postController.getPostById);
-
-// POST /api/posts - Create new post
-postRouter.post('/', postController.createPost);
-
-// PUT /api/posts/:id - Update post
-postRouter.put('/:id', postController.updatePost);
-
-// DELETE /api/posts/:id - Delete post
-postRouter.delete('/:id', postController.deletePost);
 
 export default postRouter;
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,19 +3,4 @@ import userController from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
-// GET /api/users - Get all users
-userRouter.get('/', userController.getAllUsers);
-
-// GET /api/users/:id - Get user by ID
-userRouter.get('/:id', userController.getUserById);
-
-// POST /api/users - Create new user
-userRouter.post('/', userController.createUser);
-
-// PUT /api/users/:id - Update user
-userRouter.put('/:id', userController.updateUser);
-
-// DELETE /api/users/:id - Delete user
-userRouter.delete('/:id', userController.deleteUser);
-
 export default userRouter;
