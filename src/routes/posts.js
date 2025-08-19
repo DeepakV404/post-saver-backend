19a0938eea2e4656a86731b4dadaedcaef1047d5
Deepak <17deepakv5@gmail.com>
2025-08-19 22:36:19 +0530

    chore: init user + post routes

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,19 +2,5 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
-// GET /api/posts - Get all posts
-router.get('/', postController.getAllPosts);
-
-// GET /api/posts/:id - Get post by ID
-router.get('/:id', postController.getPostById);
-
-// POST /api/posts - Create new post
-router.post('/', postController.createPost);
-
-// PUT /api/posts/:id - Update post
-router.put('/:id', postController.updatePost);
-
-// DELETE /api/posts/:id - Delete post
-router.delete('/:id', postController.deletePost);
 
 module.exports = router;
