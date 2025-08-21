3347e256f74808e9a5aeab133f0d3c91f3ef488c
Deepak <17deepakv5@gmail.com>
2025-08-21 22:08:48 +0530

    chore: post routes

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -3,5 +3,14 @@ import postController from '../controllers/postController.js';
 
 const postRouter = express.Router();
 
+postRouter.get('/', postController.getAllPosts);
+
+postRouter.get('/:id', postController.getPostById);
+
+postRouter.post('/', postController.createPost);
+
+postRouter.put('/:id', postController.updatePost);
+
+postRouter.delete('/:id', postController.deletePost);
 
 export default postRouter;
