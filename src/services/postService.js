d015b9cf8b205246a33e3f4ff00a3abe7f567062
Deepak <17deepakv5@gmail.com>
2025-08-21 22:26:05 +0530

    chore: init post service

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,5 +1,30 @@
 const postService = {
 
+    async getAllPosts() {
+        // TODO: Replace with actual database query
+        return [];
+    },
+
+    async getPostById(postId) {
+        // TODO: get the post by Id and return the post
+        return {};
+    },
+
+    async createPost(postData) {
+        // TODO: create a new post and return the created post
+        
+        return {}
+    },
+
+    async updatePost(postId, updateData) {
+        // TODO: update post with updateData and return the updated post
+        return {};
+    },
+
+    async deletePost(postId) {
+        // TODO: delete the post and return success status
+        return true;
+    }
 };
 
 export default postService;
